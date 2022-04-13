65dd4731b4768a4ab0860bf6a17473518baa7299
Sophie.Chan <sophie.chan@bjss.com>
2022-04-13 15:27:46 +0100

    changed comments

--- main.java
+++ main.java
@@ -2,9 +2,9 @@ public class main {
     public static void main(String [] args) {
 //get shape factory
 abstractfactory shapeFactory = factoryproducer.getFactory(false);
-//get an object of Shape Rectangle
+//get an object of Shape circle
 shape shape1 = shapeFactory.getShape("circle");
-//call draw method of Shape Rectangle
+//call draw method of Shape circle
 shape1.draw();
 }
 }
